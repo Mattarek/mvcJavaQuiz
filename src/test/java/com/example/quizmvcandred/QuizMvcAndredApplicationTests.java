4a75dac6257180d1fd15efc326253fde01bf36e2
Marcin Barć <marcin.barc.programowanie@gmail.com>
2025-12-14 14:52:19 +0100

    first commit

---
+++ src/test/java/com/example/quizmvcandred/QuizMvcAndredApplicationTests.java
@@ -0,0 +1,12 @@
+package com.example.quizmvcandred;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizMvcAndredApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+}
